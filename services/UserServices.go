19cbb5fe48b67dc6892067f15e372b3384f3ae42
Ashish Kumar Bhoi <ashish.bhoi12@gmail.com>
2023-07-26 17:25:44 +0530

    Modified UserServices.go to return jwt token

--- services/UserServices.go
+++ services/UserServices.go
@@ -1,21 +1,39 @@
 package services
 
 import (
-	"github.com/ashishbhoi/go-crud/database"
 	"github.com/ashishbhoi/go-crud/models"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"net/http"
+	"os"
+	"time"
 )
 
 type userModel struct {
+	ID        uint   `json:"id"`
 	FirstName string `json:"firstname"`
 	LastName  string `json:"lastname"`
 	Email     string `json:"email"`
 }
 
+func jwtToken(user userModel) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":        user.ID,
+		"firstname": user.FirstName,
+		"lastname":  user.LastName,
+		"email":     user.Email,
+		"exp":       time.Now().Add(time.Hour * 24).Unix(), // 1 day
+	})
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
 func CheckUser(email string) bool {
 	var user models.User
-	database.DB.Where("email = ?", email).First(&user)
+	models.DB.Where("email = ?", email).First(&user)
 	if user.Email == email {
 		return true
 	}
@@ -27,16 +45,21 @@ func CreateUser(context *gin.Context) {
 	var user models.User
 	err := context.BindJSON(&user)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	if CheckUser(user.Email) {
-		context.JSON(http.StatusConflict, gin.H{"message": "User Already Exists"})
+		context.AbortWithStatusJSON(http.StatusConflict, gin.H{"message": "User Already Exists"})
 		return
 	}
-	database.DB.Create(&user)
-	displayUser := userModel{user.FirstName, user.LastName, user.Email}
-	context.JSON(201, gin.H{"message": "User Created Successfully", "user": displayUser})
+	models.DB.Create(&user)
+	displayUser := userModel{user.ID, user.FirstName, user.LastName, user.Email}
+	token, err := jwtToken(displayUser)
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	context.JSON(http.StatusCreated, gin.H{"message": "User Created Successfully", "token": token})
 }
 
 func VerifyUser(context *gin.Context) {
@@ -45,14 +68,19 @@ func VerifyUser(context *gin.Context) {
 	var verifyUser models.User
 	err := context.BindJSON(&verifyUser)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	database.DB.Where("email = ?", verifyUser.Email).First(&user)
+	models.DB.Where("email = ?", verifyUser.Email).First(&user)
 	if user.Email == verifyUser.Email && user.Password == verifyUser.Password {
-		displayUser := userModel{user.FirstName, user.LastName, user.Email}
-		context.JSON(http.StatusAccepted, gin.H{"message": "User Verified", "user": displayUser})
+		displayUser := userModel{user.ID, user.FirstName, user.LastName, user.Email}
+		token, err := jwtToken(displayUser)
+		if err != nil {
+			context.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		context.JSON(http.StatusAccepted, gin.H{"message": "User Verified", "token": token})
 	} else {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "User Not Verified"})
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "User Not Verified"})
 	}
 }
